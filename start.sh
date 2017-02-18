c34a411aee41d9c789b3b57d9a224dea81cd18ac
meti <meti@lplab.net>
2017-02-18 09:29:58 +0100

    Change start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-docker run --name bind5 -d --restart=always   --publish 10.1.2.158:53:53/tcp --publish 10.1.2.158:53:53/udp --publish 10.1.2.158:10000:10000/tcp --volume /srv/docker/bind:/data bind9:latest
+docker run --name bind -d --restart=always   --publish 10.1.2.158:53:53/tcp --publish 10.1.2.158:53:53/udp --publish 10.1.2.158:10000:10000/tcp --volume /srv/docker/bind:/data bind9:latest
